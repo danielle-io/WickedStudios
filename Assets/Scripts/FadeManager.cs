4169851486e6ed37ebeb3ea07c972b9aceadd529
Danielle Muhlenberg <dmuhlenb@uci.edu>
2019-11-19 23:52:39 -0800

    Set up main menu skeleton

---
+++ Assets/Scripts/FadeManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace WickedStudios
+{
+    public class FadeManager : MonoBehaviour
+    {
+        private Text fadingImage;
+        void Awake()
+        {
+            fadingImage = GetComponent<Text>();
+            fadingImage.CrossFadeAlpha(0, 2.5f, false);
+            StartCoroutine(Timer());
+        }
+
+        IEnumerator Timer()
+        {
+            fadingImage.CrossFadeAlpha(0, 2.5f, false);
+            yield return new WaitForSeconds(2.5f);
+            Destroy(gameObject);
+        }
+    }
+}
+
---
+++ Assets/Scripts/Quit.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WickedStudios
+{
+    public class Quit : MonoBehaviour
+    {
+
+        public void quitter()
+        {
+            //quits application
+            Application.Quit();
+        }
+    }
+}
+
