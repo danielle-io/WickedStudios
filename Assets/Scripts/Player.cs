3627ab5a6ba918399b31c77befda5104095f1154
Rosy Pen <rosy.pen.1@gmail.com>
2019-11-24 18:56:57 -0800

    Added Jane with Paper animations (made Boss box collider a trigger to transition)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,12 +7,33 @@ namespace WickedStudios
 {
     public class Player : MonoBehaviour
     {
+        private Animator animator;
         public float speed = 4;
+
+        void Start()
+        {
+            animator = GetComponent<Animator> ();
+        }
 
         void Update()
         {
             var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
             transform.position += move * speed * Time.deltaTime;
+        }
+
+        //OnTriggerEnter2D is sent when another object enters a trigger collider attached to this object (2D physics only).
+		private void OnTriggerEnter2D (Collider2D other)
+		{
+			//Check if the tag of the trigger collided with is Exit.
+			if(other.tag == "Paper")
+			{
+				animator.SetTrigger("janeWithPaper");
+			}
+
+            if(other.tag == "Boss")
+            {
+                animator.SetTrigger("janeForward");
+            }
         }
 
     }
