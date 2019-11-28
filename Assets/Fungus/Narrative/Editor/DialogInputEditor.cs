8fbe299c047ea5b5a0a13b5cf547babdf0276030
Danielle Muhlenberg <dmuhlenb@uci.edu>
2019-11-27 20:06:50 -0800

    Set up intro scene animations, re-installed library, worked on dialogue

--- Assets/Fungus/Narrative/Editor/DialogInputEditor.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-/// <summary>
-/// Dummy class to avoid compile errors for users upgrading from Fungus 2 to Fungus 3 via unitypackage.
-/// </summary>
-public class DialogInputEditor
-{}
--- Assets/Fungus/Thirdparty/CSVParser/CsvParser.cs
+++ Assets/Fungus/Thirdparty/CSVParser/CsvParser.cs
@@ -1,7 +1,13 @@
-﻿using System;
+﻿// Copyright 2014 ideafixxxer
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Ideafixxxer.CsvParser
 {
@@ -190,28 +196,36 @@ namespace Ideafixxxer.CsvParser
 			var context = new ParserContext();
 
 			// Handle both Windows and Mac line endings
-			string[] lines = csvData.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+			var lines = Regex.Split(csvData, "\n|\r\n");
 
 			ParserState currentState = ParserState.LineStartState;
-			foreach (string next in lines)
-			{
-				foreach (char ch in next)
+			for (int i = 0; i < lines.Length; i++) {
+				var next = lines [i];
+
+				// Skip empty entries
+				if (next.Length == 0)
 				{
-					switch (ch)
-					{
+					continue;
+				}
+
+				for (int j = 0; j < next.Length; j++) {
+					var ch = next [j];
+
+					switch (ch) {
 					case CommaCharacter:
-						currentState = currentState.Comma(context);
+						currentState = currentState.Comma (context);
 						break;
 					case QuoteCharacter:
-						currentState = currentState.Quote(context);
+						currentState = currentState.Quote (context);
 						break;
 					default:
-						currentState = currentState.AnyChar(ch, context);
+						currentState = currentState.AnyChar (ch, context);
 						break;
 					}
 				}
-				currentState = currentState.EndOfLine(context);
+				currentState = currentState.EndOfLine (context);
 			}
+
 			List<string[]> allLines = context.GetAllLines();
 			return allLines.ToArray();
 		}
