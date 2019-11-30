33151d007fb1b4aeab3b1e1a2363d848baf171f8
Danielle Muhlenberg <dmuhlenb@uci.edu>
2019-11-29 16:45:41 -0800

    Wrote intro dialogue, added in pixel boxes, added intro music, worked on design.

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -5,29 +5,24 @@ namespace WickedStudios
 {
 	public class SoundManager : MonoBehaviour 
 	{
-        //Drag a reference to the audio source which will play the sound effects.
         public AudioSource efxSource;		
 
         //Drag a reference to the audio source which will play the music.
         public AudioSource musicSource;					
-		public static SoundManager instance = null;		//Allows other scripts to call functions from SoundManager.				
-		public float lowPitchRange = .95f;				//The lowest a sound effect will be randomly pitched.
-		public float highPitchRange = 1.05f;			//The highest a sound effect will be randomly pitched.
+		public static SoundManager instance = null;		
+		public float lowPitchRange = .95f;				
+		public float highPitchRange = 1.05f;			
 		
 		
 		void Awake ()
 		{
-			//Check if there is already an instance of SoundManager
-			if (instance == null)
-				//if not, set it to this.
-				instance = this;
-			//If instance already exists:
-			else if (instance != this)
-				//Destroy this, this enforces our singleton pattern so there can only be one instance of SoundManager.
-				Destroy (gameObject);
+			//if (instance == null)
+			//	instance = this;
+			//else if (instance != this)
+				//Destroy (gameObject);
 			
 			//Set SoundManager to DontDestroyOnLoad so that it won't be destroyed when reloading our scene.
-			// DontDestroyOnLoad (gameObject);
+			 DontDestroyOnLoad (gameObject);
 		}
 		
 		
